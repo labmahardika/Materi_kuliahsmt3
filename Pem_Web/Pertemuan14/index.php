03efb16863097e501a537255203c5dce2e2898d6
labmahardika <labkommahardika635@gmail.com>
2024-12-16 11:53:20 +0700

    CRUD FORM

--- Pem_Web/Pertemuan14/index.php
+++ Pem_Web/Pertemuan14/index.php
@@ -2,28 +2,55 @@
 // Memnaggil File dbConnection.php sebagai Global Var
 include "dbConnection.php";
 // Membuat Operasi Delete Data
-
 if (isset($_GET['delete'])) {
     $nim = $_GET['delete'];
-    $sql = "DELETE FROM tbl_mahasiswa WHERE nim = $nim";
+    $sql = "DELETE FROM tbl_mahasiswa WHERE nim = '$nim'";
+    $connect->query($sql);
+    header("Location: index.php");
+    exit();
+}
+// Membuat Operasi Insert dan Update
+$editData = null;
+if (isset($_GET['edit'])) {
+    $nim = $_GET['edit'];
+    $result = $connect->query("SELECT * FROM tbl_mahasiswa WHERE nim = $nim");
+    if ($result->num_rows > 0) {
+        $editData = $result->fetch_assoc();
+    }
+}
+// Membuat Operasi Insert dan Update
+if (isset($_POST['submit'])) {
+    $nim = $_POST['nim'];
+    $nama = $_POST['nama'];
+    $jurusan = $_POST['jurusan'];
+    if (isset($_POST['nim']) && $_POST['nim'] != "") {
+        $nim = $_POST['nim'];
+        $sql = "UPDATE tbl_mahasiswa SET nama = '$nama', jurusan = '$jurusan' WHERE nim = $nim";
+    } else {
+        $sql = "INSERT INTO tbl_mahasiswa (nim, nama, jurusan) VALUES ('$nim', '$nama', '$jurusan')";
+    }
     $connect->query($sql);
     header("Location: index.php");
     exit();
 }
 // Membuat Form Mahasiwa
-function formMahasiswa()
+function formMahasiswa($data = null)
 {
+    $id = $data ? $data['nim'] : '';
+    $name = $data ? $data['nama'] : '';
+    $jurusan = $data ? $data['jurusan'] : '';
+    $buttonLabel = $data ? 'Edit' : 'Submit';
     echo "
 <h1>Form Mahasiswa</h1>
 <form  method='POST'>
 <table border='1'>
-<tr><td><label for='nim'>NIM</label></td>
-    <td><input type='text' name='nim' id='nim'></td></tr>
+<tr><td><label for='nim' >NIM</label></td>
+    <td><input type='text' value='$id' name='nim' id='nim'></td></tr>
 <tr><td><label for='nama'>NAMA</label></td>
-    <td><input type='text' name='nama' id='nama'></td></tr>
+    <td><input type='text' name='nama' value='$name' id='nama'></td></tr>
 <tr><td><label for='jurusan'>JURUSAN</label></td>
-    <td><input type='text' name='jurusan' id='jurusan'></td></tr>
-<tr><td></td><td><input type='submit' value='Submit'></td></tr>
+    <td><input type='text' name='jurusan' value='$jurusan' id='jurusan'></td></tr>
+<tr><td></td><td><button type='submit' name='submit'>$buttonLabel</button></td></tr>
 </table>
 </form>";
 }
@@ -51,7 +78,7 @@ function tampilTabel($connect)
             <td>{$row['nama']}</td>
             <td>{$row['jurusan']}</td>
             <td><a href='?delete={$row['nim']}' onClick='return confirm(`Are you sure?`)'>Delete</a>
-            <a href='?update={$row['nim']}'>Update</a></td>
+            <a href='?edit={$row['nim']}'>Edit</a></td>
             </tr>
             ";
             $no++;
@@ -62,5 +89,5 @@ function tampilTabel($connect)
     echo "</table>";
 }
 // Panggil Function
-formMahasiswa();
+formMahasiswa($editData);
 tampilTabel($connect);
