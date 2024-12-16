1756e6f7c71692a4b2cda88886d23238171274fe
labmahardika <labkommahardika635@gmail.com>
2024-12-16 10:36:39 +0700

    db

---
+++ Pem_Web/Pertemuan14/dbConnection.php
@@ -0,0 +1,7 @@
+<?php
+$connect = mysqli_connect("localhost", "root", "", "sikampus");
+if (!$connect) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+} else {
+    echo "Koneksi Berhasil";
+}
